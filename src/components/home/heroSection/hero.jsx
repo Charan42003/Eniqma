389b9ce1d87cfcf230ea49fd649302df08e39f6c
Charan Raj <charanrajs4122003@gmail.com>
2023-11-02 12:49:41 +0530

    Hero BgUpdate

---
+++ src/components/home/heroSection/hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import heroImg from '../../../assets/background-image.jpg'
+
+function Hero() {
+  return (
+    <>
+    <img src={ heroImg } alt="" className='w-full h-full opacity-30' />
+    </>
+  )
+}
+
+export default Hero
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Nav from './navbarSection/nav'
+import Hero from './heroSection/hero'
 
 function Home() {
   return (
     <>
     <div className="container-custom mx-auto">
+        {/* Nav bar  */}
         <Nav />
+
+        {/* Hero Section  */}
+        <Hero />
     </div>
     </>
   )
