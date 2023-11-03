e5c0b472d8a6fb2ff1324dd7a73682b6b03106b1
Charan Raj <charanrajs4122003@gmail.com>
2023-11-03 13:43:00 +0530

    header update

--- src/components/home/heroSection/hero.jsx
+++ src/components/home/heroSection/hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import earth from '../../../assets/earth.jpg'
+import earth from '../../../assets/globe1.png'
 
 function Hero() {
   return (
@@ -8,19 +8,22 @@ function Hero() {
         <div className="hero-shadow object-contain min-h-[100vh] max-h-[100vh] w-full">
         </div>
         <div className="relative z-10 mt-14  flex flex-col items-center  hero-content">
-          <h1 className="text-white text-[70px] font-bold">
-            Trending 
+          <h1 className="text-white text-[60px] font-bold">
+            Unleash your creativity at <br />
             <span className="">
-              Title
+              The East Point - Eniqma
             </span>
           </h1>
-          <p className='text-grey w-[60%] text-center mt-4'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          <p className='text-grey text-[20px] w-[60%] text-center mt-4'>
+          Where Geeks And Prodigies create Magic
           </p>
           <div className="mt-5">
             <button className="font-bold text-[20px] tracking-widest !border-transparent register-btn">
               Register Now
             </button>
+            {/* <button className="font-bold text-[20px] tracking-widest !border-transparent register-btn">
+              Join Community
+            </button> */}
           </div>
         </div>
         <img src={ earth } alt="" className='hero-earth' />
--- src/components/home/navbarSection/nav.jsx
+++ src/components/home/navbarSection/nav.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import logo from '../../../assets/logo.png'
+import logo from '../../../assets/logo1.png'
 function Nav() {
     const [close, setClose] = useState(true);
     const [isOpen, setIsOpen] = useState(false);
